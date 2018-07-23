90b911b5c1b84a6f96ca19c4ea4f3d6f63b53453
bablue <mitrabhaskar1989@gmail.com>
2018-07-24 02:37:08 +0530

    startingfiles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Items from './components/items.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <Items></Items>
+    );
+  }
+}
+
+export default App;
